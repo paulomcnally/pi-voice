1ee139ea744e47eab95b6cd2608f42a634010d6a
Paulo McNally <paulomcnally@gmail.com>
2019-02-09 23:55:26 -0600

    Fix match

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ try:
                 if match:
                     os.system("omxplayer /home/pi/pi-voice/locales/es/ok.mp3")
                     Action = getattr(importlib.import_module('actions.%s' % command['class_name'].lower()), command['class_name'])
-                    Action.run(match.group(2))
+                    Action.run(match.group())
                     break
 
             # No valid action found
